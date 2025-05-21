2188cdbed023ba43698647b39c46985e001cc370
BTW-AAK <aditamitkumar@gmail.com>
2025-05-22 01:16:45 +0530

    Fix IST timezone

--- app/db.py
+++ app/db.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-import time
+import datetime
+import pytz
 
 db = SQLAlchemy()
+ist_timezone = pytz.timezone('Asia/Kolkata')
 
 class EnergyUsage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     device_id = db.Column(db.String(100))
-    timestamp = db.Column(db.DateTime, default=time.localtime())
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.now(ist_timezone))
     energy_wh = db.Column(db.Float)
     voltage = db.Column(db.Float)
     current = db.Column(db.Float)
